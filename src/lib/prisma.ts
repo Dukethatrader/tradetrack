95f5875b738931792f0c03b2e76b754e741aaeac
Dukethatrader <masterjosiah88@gmai.com>
2026-03-29 23:45:26 +0100

    Revert Prisma client to use native Prisma engine

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const connectionString = `${process.env.DATABASE_URL}`.replace('5432', '6543').split('?')[0] + '?sslmode=require'
-
-const pool = new Pool({ 
-  connectionString,
-  ssl: { rejectUnauthorized: false }
-})
-const adapter = new PrismaPg(pool as any)
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 declare const globalThis: {
